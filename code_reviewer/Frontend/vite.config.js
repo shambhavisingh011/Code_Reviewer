8e8741a58a0117753c0101ca5f65cf851204dff8
Shambhavi Singh <121101577+shambhavisingh011@users.noreply.github.com>
2025-07-06 00:58:26 +0530

    Update vite.config.js

--- code_reviewer/Frontend/vite.config.js
+++ code_reviewer/Frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite' //Imports a helper function defineConfig from Vite.It helps with auto-complete and type checking if you're using TypeScript or an IDE.
+import react from '@vitejs/plugin-react' //Imports the official React plugin for Vite.
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({ //Exports the configuration using defineConfig() so Vite can read it.
+  plugins: [react()], //Tells Vite: “Use the React plugin.”Understand .jsx/.tsx files Properly build the React app
 })
+//Hey, I’m building a React app — use the React plugin to make everything work properly
